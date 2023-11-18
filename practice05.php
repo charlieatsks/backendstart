a4e92fa9a4763c44c0d471a14f9b61c0e3d05100
charlieatsks <148255171+charlieatsks@users.noreply.github.com>
2023-11-18 23:04:09 +0530

    Add files via upload

---
+++ practice05.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+Loops - 
+For loop
+While loop
+Do While loop
+*/
+
+
+<?php
+
+for($index = 0; $index < 5; $index ++)
+{
+    echo "Current loop counter ".$index.".\n";
+}
+?>
+
+
+<?php
+$index = 10;
+while ($index >= 0)
+{
+    echo "The index is ".$index.".\n";
+    $index--;
+}
+?>
+
+
+<?php
+$index = 3;
+do
+{
+    // execute this at least 1 time
+    echo "Index: ".$index.".\n"; 
+    $index --;
+}
+while ($index > 0);
+?>
+
+
+
+
+
