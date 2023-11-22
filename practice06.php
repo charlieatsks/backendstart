87fbcb2f07e0a502c3ee2ef84a2c789197348274
charlieatsks <148255171+charlieatsks@users.noreply.github.com>
2023-11-22 23:21:29 +0530

    Add files via upload

---
+++ practice06.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Arrays
+Two types of arrays: Indexed arrays and Associative arrays.
+*/
+
+<?php
+$shopping_list = array("eggs", "milk", "cheese");
+?>
+
+//Associative Array
+<?php
+$student_scores = array("Joe" => 83, "Frank" => "93", "Benji" => "90");
+?>
+
+//Multidimensional Array
+<?php
+$students = 
+  array(
+    array("first_name" => "Joe", "score" => 83, "last_name" => "Smith"),
+    array("first_name" => "Frank", "score" => 92, "last_name" => "Barbson"),
+    array("first_name" => "Benji", "score" => 90, "last_name" => "Warner")   
+  );
+?>
+
+
+
